0b0304f57eccda10795fb0748db640bdbb7fd4a8
Eric Sproul <esproul@omniti.com>
2013-05-03 20:43:19 +0000

    Basic cpu stats for illumos, with output matching that on Linux
    Signed-off-by: Eric Sproul <esproul@omniti.com>

---
+++ plugins/illumos/src/cpu.c
@@ -0,0 +1,57 @@
+#include <kstat.h>  
+#include <stdio.h>
+#include <strings.h>
+#include <inttypes.h>
+#include <sys/sysinfo.h>
+
+#define CSSUM(name) sum.cpu_sysinfo.name += cpu.cpu_sysinfo.name
+
+int main(int argc, char **argv) {
+  kstat_ctl_t   *kc;  
+  kstat_t       *ksp;  
+  kstat_io_t     kio;  
+  kstat_named_t *knp;
+  int ncpus = 0;
+  cpu_stat_t sum;
+  cpu_stat_t cpu;
+ 
+  memset(&sum, 0, sizeof(sum)); 
+  kc = kstat_open();  
+  ksp = kstat_lookup(kc, "cpu_stat", -1, NULL);
+  for (; ksp != NULL; ksp = ksp->ks_next) { 
+    if(!strcmp(ksp->ks_module, "cpu_stat")) {
+      ncpus++;
+      kstat_read(kc,ksp,&cpu);
+      CSSUM(cpu[CPU_IDLE]);
+      CSSUM(cpu[CPU_USER]);
+      CSSUM(cpu[CPU_KERNEL]);
+      CSSUM(cpu[CPU_WAIT]);
+      CSSUM(wait[W_IO]);
+      CSSUM(intr);
+      CSSUM(inv_swtch);
+      CSSUM(pswitch);
+      CSSUM(syscall);
+    }
+  }
+
+  /*
+   * Some stats are not implemented (yet), so make them zero.
+   */
+  printf("%s\tL\t%llu\n", "user", sum.cpu_sysinfo.cpu[CPU_USER]/ncpus);
+  printf("%s\tL\t%llu\n", "user`normal", sum.cpu_sysinfo.cpu[CPU_USER]/ncpus);
+  printf("%s\tL\t%llu\n", "user`nice", 0);
+  printf("%s\tL\t%llu\n", "kernel", sum.cpu_sysinfo.cpu[CPU_KERNEL]/ncpus);
+  printf("%s\tL\t%llu\n", "kernel`sys", sum.cpu_sysinfo.cpu[CPU_KERNEL]/ncpus);
+  printf("%s\tL\t%llu\n", "kernel`guest", 0);
+  printf("%s\tL\t%llu\n", "kernel`guest_nice", 0);
+  printf("%s\tL\t%llu\n", "idle", sum.cpu_sysinfo.cpu[CPU_IDLE]/ncpus);
+  printf("%s\tL\t%llu\n", "idle`normal", sum.cpu_sysinfo.cpu[CPU_IDLE]/ncpus);
+  printf("%s\tL\t%llu\n", "idle`steal", 0);
+  printf("%s\tL\t%llu\n", "wait_io", sum.cpu_sysinfo.wait[W_IO]);
+  printf("%s\tL\t%llu\n", "intr", sum.cpu_sysinfo.intr);
+  /* We do not distinguish hard from soft interrupts on illumos */
+  printf("%s\tL\t%s\n", "intr`hard", "[[null]]");
+  printf("%s\tL\t%s\n", "intr`soft", "[[null]]");
+  printf("%s\tL\t%llu\n", "context_switch", (sum.cpu_sysinfo.inv_swtch + sum.cpu_sysinfo.pswitch));
+  printf("%s\tL\t%llu\n", "syscall", sum.cpu_sysinfo.syscall);
+}  
