29b000eec3736bf95ac8e387df747ac38a3ffc41
Eric Sproul <esproul@omniti.com>
2013-07-12 21:52:02 +0000

    Start of FreeBSD support
    CPU counters are a bit different here. Statistics are gathered on a
    secondary timer (stathz) whose value can vary based on kern.hz but
    aims for 128 Hz.  In order for these numbers to approximate the values
    seen on other platforms, we must take this timer into account.

---
+++ plugins/freebsd/cpu.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+#
+# CPU usage statistics, via sysctl
+
+# Print ordinary metrics
+print_cssum() {
+    printf "%s\tL\t%s\n" $1 $2
+}
+
+# Print metrics normalized to a single CPU and 100Hz tick rate
+print_norm_cssum() {
+    per_cpu_count=`expr $2 / $NCPUS`
+    rate_factor=`echo "scale=2; 100/$STATHZ" | bc`
+    value=`echo "$per_cpu_count*$rate_factor" | bc`
+    printf "%s\tL\t%.0f\n" $1 $value
+}
+
+NCPUS=`sysctl -n hw.ncpu`
+STATHZ=`sysctl -n kern.clockrate | awk '{ print $13 }'`
+
+ALLCPU=`sysctl -n kern.cp_time`
+CPU_USER_NORMAL=`echo $ALLCPU | cut -d' ' -f1`
+CPU_USER_NICE=`echo $ALLCPU | cut -d' ' -f2`
+CPU_SYS=`echo $ALLCPU | cut -d' ' -f3`
+CPU_IDLE_NORMAL=`echo $ALLCPU | cut -d' ' -f5`
+
+# Interrupts come from vm stats 
+CPU_IRQ=`sysctl -n vm.stats.sys.v_intr`
+CPU_SOFTIRQ=`sysctl -n vm.stats.sys.v_soft`
+
+# Not implemented
+CPU_WAIT_IO=0
+CPU_STEAL=0
+CPU_GUEST=0
+CPU_GUEST_NICE=0
+
+# Summarize interrupts
+CPU_INTR=`expr $CPU_IRQ + $CPU_SOFTIRQ`
+
+# Summarize kernel time
+#
+# "guest" and "guest_nice" are time spent running virtual CPUs, and count as
+# kernel time
+CPU_KERNEL=`expr $CPU_SYS + $CPU_GUEST + $CPU_GUEST_NICE`
+
+# Summarize idle time
+#
+# "steal" is time while we, a guest, are runnable but a real CPU isn't
+# servicing our virtual CPU
+CPU_IDLE=`expr $CPU_IDLE_NORMAL + $CPU_STEAL`
+
+# Summarize user time
+CPU_USER=`expr $CPU_USER_NORMAL + $CPU_USER_NICE`
+
+# Context switches
+CTXT=`sysctl -n vm.stats.sys.v_swtch`
+
+# System calls
+SYSCALL=`sysctl -n vm.stats.sys.v_syscall`
+
+print_norm_cssum user $CPU_USER
+print_norm_cssum user\`normal $CPU_USER_NORMAL
+print_norm_cssum user\`nice $CPU_USER_NICE
+print_norm_cssum kernel $CPU_KERNEL
+print_norm_cssum kernel\`sys $CPU_SYS
+print_norm_cssum kernel\`guest $CPU_GUEST
+print_norm_cssum kernel\`guest_nice $CPU_GUEST_NICE
+print_norm_cssum idle $CPU_IDLE
+print_norm_cssum idle\`normal $CPU_IDLE_NORMAL
+print_norm_cssum idle\`steal $CPU_STEAL
+print_norm_cssum wait_io $CPU_WAIT_IO
+print_norm_cssum intr $CPU_INTR
+print_norm_cssum intr\`hard $CPU_IRQ
+print_norm_cssum intr\`soft $CPU_SOFTIRQ
+print_cssum context_switch $CTXT
+print_cssum syscall $SYSCALL
