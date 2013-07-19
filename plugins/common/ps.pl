b3d695da2ef9c7edf944ba96d1118eca0106d78e
Clinton Wolfe <clinton@omniti.com>
2013-07-19 18:12:46 +0200

    Optionally restrict ps outbut by a list of users

--- plugins/common/ps.pl
+++ plugins/common/ps.pl
@@ -6,24 +6,24 @@ use warnings;
 # We want...
 # total processes
 # process count by state
-# Parametrically report above restricted by username
+# Parametrically report above restricted by username(s)
 
-my $username_opt = '';
+my $username_opt = @ARGV ? "-u " . join(',', @ARGV)  : '-e ';
 
 # We need: user, a pid, and state
 #    no header please
 #   -o pid= -o user= -o s=
 #  above works on solaris and gnu ps
+my $cmd = "ps -o pid= -o user= -o s= $username_opt";
+print "Have command: '$cmd'\n";
 
-my @ps_output = `ps -e -o pid= -o user= -o s= $username_opt`;
-my %count_by_state = map { $_ => 0 } qw(D R S Z O); # Others?
-for my $proc (@ps_output) {
+my %count_by_state = map { $_ => 0 } qw(D R S Z O total); # Others?
+for my $proc (`$cmd`) {
     $proc =~ s/^\s+//;
     my ($pid, $user, $state) = split(/\s+/, $proc);
     $count_by_state{$state}++;
+    $count_by_state{total}++;
 }
-
-$count_by_state{total} = scalar @ps_output;
 
 for my $kind (keys %count_by_state) {
     print "count`$kind\t\L\t" . $count_by_state{$kind} . "\n";
