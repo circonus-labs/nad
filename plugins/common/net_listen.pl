529b9f49141963a4c67b35692384e6b15825902e
Clinton Wolfe <clinton@omniti.com>
2013-07-20 00:21:54 +0200

    hrm, pfile hunt for listening process

--- plugins/common/net_listen.pl
+++ plugins/common/net_listen.pl
@@ -37,6 +37,7 @@ if (@ARGV) {
 if ($portspec !~ /:/) {
     $portspec = $YAY_PORTABILITY{$^O}{all_iface} . ':'. $portspec;
 }
+my ($sought_ip, $sought_port) = split(':', $portspec);
 $portspec = join($YAY_PORTABILITY{$^O}{separator}, split(':', $portspec));
 
 my $cmd = $YAY_PORTABILITY{$^O}{netstat};
@@ -56,7 +57,27 @@ foreach my $listener (`$cmd`) {
     last;
 }
 
-# TODO solaris pfiles hunt?
+# Solaris pfiles hunt?
+if ($^O eq 'solaris') {
+    unless ($>) {
+        # I'm root, it's worth a try
+        my $matcher = $sought_ip eq $YAY_PORTABILITY{$^O}{all_iface} ? 
+          /sockname: AF_INET.+port: $sought_port/ :
+            /sockname: AF_INET.+$sought_ip\s+port: $sought_port/;
+
+      PROC:
+        foreach my $cpid (`ps -e -o pid=`) {
+          LINE:
+            foreach my $line (`pfiles $cpid`) {
+                # sockname: AF_INET6 ::ffff:10.0.2.15  port: 22
+                if ($line =~ $matcher) {
+                    $pid = $cpid;
+                    last PROC;
+                }
+            }
+        }
+    }
+}
 
 print "listening\tI\t$matched\n";
 if ($pid) {
