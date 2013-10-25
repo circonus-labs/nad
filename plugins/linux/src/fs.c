1ec2d95bc77548e7eb67db28b4c9dbe6660d06c9
Theo Schlossnagle <jesus@omniti.com>
2013-10-25 18:01:29 -0400

    provide % used numbers directly

--- plugins/linux/src/fs.c
+++ plugins/linux/src/fs.c
@@ -31,11 +31,16 @@ int main(int argc, char **argv) {
       if(!strcmp(mnt.mnt_type, suppress_fstype[i])) break;
 
     if (suppress_fstype[i] == NULL && statvfs(mnt.mnt_dir, &buf) == 0) {
+      long long unsigned int used = buf.f_blocks - buf.f_bfree,
+                             adj = buf.f_blocks - buf.f_bfree + buf.f_bavail;
+      double pct = (used * 100) / adj + ((used * 100) % adj != 0);
       printf("%s`f_bsize\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_bsize);
       printf("%s`f_frsize\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_frsize);
       printf("%s`f_blocks\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_blocks);
       printf("%s`f_bfree\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_bfree);
       printf("%s`f_bavail\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_bavail);
+      printf("%s`df_used_percent\tL\t%0.2f\n", mnt.mnt_dir, pct);
+      printf("%s`used_percent\tL\t%0.2f\n", mnt.mnt_dir, 100.0*(double)used/(double)adj);
       printf("%s`f_files\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_files);
       printf("%s`f_ffree\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_ffree);
       printf("%s`f_favail\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_favail);
