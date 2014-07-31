e86877959374f770c332c12818cabfb95ad287ab
Eric Sproul <esproul@omniti.com>
2014-07-31 16:01:26 +0000

    Zpool error counters must account for K/M/G units

--- plugins/common/zpool.sh
+++ plugins/common/zpool.sh
@@ -11,25 +11,44 @@ PATH="/usr/xpg4/bin:$PATH"
 pools=`/sbin/zpool list -H -o name`
 
 for zp in $pools; do \
-    /sbin/zpool status $zp | awk -v poolname=$zp '{
-        if ($1 ~ /(pool:|scan:|config:|errors:|NAME|^logs$|^cache$|^$)/) {
-            next;
+    /sbin/zpool status $zp | awk -v poolname=$zp '
+        function convert (val)
+        {
+            if (val ~ /K$/) { val = val*1000 }
+            if (val ~ /M$/) { val = val*1000000 }
+            if (val ~ /G$/) { val = val*1000000000 }
+            return val
         }
-        if ($1 == "state:") {
-            printf("%s`state\ts\t%s\n", poolname, $2);
-            next;
-        }
-        # top-level pool stats
-        if ($1 == poolname) {
-            printf("%s`errors_read\tL\t%d\n", poolname, $3);
-            printf("%s`errors_write\tL\t%d\n", poolname, $4);
-            printf("%s`errors_cksum\tL\t%d\n", poolname, $5);
-        } else {
+        {
+            if ($1 ~ /(pool:|scan:|config:|errors:|NAME|^logs$|^cache$|^$)/)
+            {
+                next;
+            }
+            if ($1 == "state:")
+            {
+                printf("%s`state\ts\t%s\n", poolname, $2);
+                next;
+            }
+            # pool-wide stats
+            if ($1 == poolname)
+            {
+                rerr = convert($3);
+                werr = convert($4);
+                cerr = convert($5);
+                printf("%s`errors_read\tL\t%d\n", poolname, rerr);
+                printf("%s`errors_write\tL\t%d\n", poolname, werr);
+                printf("%s`errors_cksum\tL\t%d\n", poolname, cerr);
+            }
             # vdev stats
-            printf("%s`%s`state\ts\t%s\n", poolname, $1, $2);
-            printf("%s`%s`errors_read\tL\t%d\n", poolname, $1, $3);
-            printf("%s`%s`errors_write\tL\t%d\n", poolname, $1, $4);
-            printf("%s`%s`errors_cksum\tL\t%d\n", poolname, $1, $5);
-        }
-    }'
+            if ($1 != poolname && $3 ~ /[0-9]+[KMG]?/ && $4 ~ /[0-9]+[KMG]?/)
+            {
+                rerr = convert($3);
+                werr = convert($4);
+                cerr = convert($5);
+                printf("%s`%s`state\ts\t%s\n", poolname, $1, $2);
+                printf("%s`%s`errors_read\tL\t%d\n", poolname, $1, rerr);
+                printf("%s`%s`errors_write\tL\t%d\n", poolname, $1, werr);
+                printf("%s`%s`errors_cksum\tL\t%d\n", poolname, $1, cerr);
+            }
+        }'
 done
