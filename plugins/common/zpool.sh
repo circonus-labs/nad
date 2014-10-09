18c6def1a01571ed40d9ba8133ba4fb1f9ea7e2e
Eric Sproul <esproul@omniti.com>
2014-10-09 15:43:33 +0000

    Add zpool scrub, resilver metrics
    These measure minutes remaining in an active scrub or resilver.
    A value of 0 means no active scan. A value of null means the time
    remaining is greater than 30 days, so no estimate is provided.
    
    Also add a description of all zpool metrics.

--- plugins/common/zpool.sh
+++ plugins/common/zpool.sh
@@ -5,6 +5,24 @@
 # without root privileges, due to the permissions on /dev/zfs.
 # See https://github.com/zfsonlinux/zfs/issues/362 for a potential workaround
 # https://github.com/zfsonlinux/zfs/issues/434 should address privilege delegation
+#
+# Metric descriptions
+# See also the zpool man page
+#
+# [poolname]`state
+#    overall pool state
+# [poolname]`resilver
+#    minutes remaining for an ongoing resilver action
+#    0 means no resilver active, null means progress is too slow (ETR > 30 days)
+# [poolname]`scrub
+#    minutes remaining for an ongoing scrub action
+#    0 means no scrub active, null means progress is too slow (ETR > 30 days)
+# [poolname]`errors_{read,write,cksum}
+#    errors at the pool level
+# [poolname]`[vdev]`state
+#    individual vdev state
+# [poolname]`[vdev]`errors_{read,write,cksum}
+#    errors at the vdev level
 
 PATH="/usr/xpg4/bin:$PATH"
 
@@ -22,7 +40,7 @@ for zp in $pools; do \
             return val
         }
         {
-            if ($1 ~ /(pool:|scan:|config:|errors:|NAME|^logs$|^cache$|^$)/)
+            if ($1 ~ /(pool:|config:|errors:|NAME|^logs$|^cache$|^$)/)
             {
                 next;
             }
@@ -30,6 +48,56 @@ for zp in $pools; do \
             {
                 printf("%s`state\ts\t%s\n", poolname, $2);
                 next;
+            }
+            if ($1 == "scan:")
+            {
+                if ($0 ~ /in progress/)
+                {
+                    # If a scrub or resilver is active, the next line will matter
+                    # scan type is either scrub or resilver, never both
+                    if ($2 == "scrub")
+                        scrub_active = "true"
+                    if ($2 == "resilver")
+                        resilver_active = "true"
+        
+                    next
+                }
+                else
+                {
+                    # no scrub or resilver active
+                    printf("%s`resilver\tL\t%s\n", poolname, 0);
+                    printf("%s`scrub\tL\t%s\n", poolname, 0);
+        
+                    next
+                }
+            }
+            # If scrub or resilver active, find the remaining time, in minutes
+            if ($2 == "scanned")
+            {
+                if ($8 ~ /[0-9]+h[0-9]+m/)
+                {
+                    split($8,t,/[hm]/)
+                    hours = t[1]
+                    mins = t[2]
+                    remain = (hours * 60) + mins
+                }
+                # remaining time > 30 days is just reported as "slow"
+                if ($0 ~ /no estimated time/)
+                    remain = "[[null]]"
+        
+                # We are either in a scrub or a resilver, never both
+                if (scrub_active)
+                {
+                    printf("%s`resilver\tL\t%s\n", poolname, 0);
+                    printf("%s`scrub\tL\t%s\n", poolname, remain);
+                }
+                if (resilver_active)
+                {
+                    printf("%s`resilver\tL\t%s\n", poolname, remain);
+                    printf("%s`scrub\tL\t%s\n", poolname, 0);
+                }
+        
+                next
             }
             # pool-wide stats
             if ($1 == poolname)
