c9afeba56117b72cb9c3dc03200f50e9b9f043b8
melcabel <melanie.abel@circonus.com>
2015-07-09 16:29:38 -0400

    Adding swap and memory percents

--- plugins/freebsd/vm.sh
+++ plugins/freebsd/vm.sh
@@ -24,6 +24,10 @@ CACHE=$(($PG_CACHE*$PGSIZE))
 INACTIVE=$(($PG_INACTIVE*$PGSIZE))
 FREE=$(($PG_FREE*$PGSIZE))
 
+MEM_USED=$(($ACTIVE+$WIRED))
+MEM_TOTAL=$(($ACTIVE+$WIRED+$CACHE+$INACTIVE+$FREE))
+MEM_PERC=`echo "scale=2;$MEM_USED/$MEM_TOTAL" | bc`
+
 # Swap
 # There doesn't seem to be a direct sysctl for swap used. Use pstat instead.
 # pstat seems to always use 'M' as the unit for swap
@@ -31,10 +35,13 @@ stats=`pstat -T | awk '$2 == "swap" { print $1; }'`
 SWAP_USED=$((`echo $stats | cut -d'/' -f1 | sed -e 's/M//'`*1048576))
 SWAP_TOTAL=$((`echo $stats | cut -d'/' -f2 | sed -e 's/M//'`*1048576))
 SWAP_FREE=$(($SWAP_TOTAL-$SWAP_USED))
+SWAP_PERC=`echo "scale=2;$SWAP_USED/$SWAP_TOTAL" | bc`
 
 print_vm memory total $(($ACTIVE+$WIRED+$CACHE+$INACTIVE+$FREE))
 print_vm memory used $(($ACTIVE+$WIRED))
 print_vm memory free $(($CACHE+$INACTIVE+$FREE))
+printf "memory\`percent_used\tn\t%0.2f\n" $MEM_PERC
 print_vm swap total $SWAP_TOTAL
 print_vm swap used $SWAP_USED
 print_vm swap free $SWAP_FREE
+printf "swap\`percent_used\tn\t%0.2f\n" $SWAP_PERC
