535e53c391ca3f5d197f7c18838eb3a657f1fbfa
melcabel <melanie.abel@circonus.com>
2015-07-09 16:26:36 -0400

    Add mempercent_used metric

--- plugins/illumos/vminfo.sh
+++ plugins/illumos/vminfo.sh
@@ -4,4 +4,15 @@ if [ -d $DIR/illumos ]; then . $DIR/illumos/lib/kstat.lib
 else . $DIR/lib/kstat.lib
 fi
 
+physmem=`_kstat_val :::physmem`
+pagesfree=`_kstat_val :::pagesfree`
+pagesused=$(($physmem-$pagesfree))
+pagesize=`pagesize`
+a=$(($physmem * $pagesize))
+b=$(($pagesused * $pagesize))
+mem_perc=`printf "%s\n" "scale = 2; $b/$a" | bc `
+
+printf "mempercent_used\tn\t%0.2f\n" $mem_perc
+
+
 _kstat -n vminfo
