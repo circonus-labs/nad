9be20ca54989b479516def18409f5055b586cc97
Sean Chittenden <seanc@groupon.com>
2015-07-28 13:00:04 +0200

    Alphasort BIN_* programs

--- plugins/freebsd/common.sh
+++ plugins/freebsd/common.sh
@@ -6,12 +6,12 @@ exec 2>&1
 set -e
 
 BIN_AWK=/usr/bin/awk
-BIN_CUT=/usr/bin/cut
-BIN_SED=/usr/bin/sed
 BIN_BC=/usr/bin/bc
+BIN_CUT=/usr/bin/cut
 BIN_EXPR=/bin/expr
 BIN_NETSTAT=/usr/bin/netstat
 BIN_PSTAT=/usr/sbin/pstat
+BIN_SED=/usr/bin/sed
 BIN_SYSCTL=/sbin/sysctl
 
 PATH_CONF=@@CONF@@
