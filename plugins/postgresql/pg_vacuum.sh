5526c6c6a7f21dfa146a1391c93a56d2882adda7
Keith Fiske <keith@keithf4.com>
2016-04-15 17:05:55 -0400

    Simplified postgres vacuum check return values

--- plugins/postgresql/pg_vacuum.sh
+++ plugins/postgresql/pg_vacuum.sh
@@ -2,9 +2,6 @@
 
 which psql >/dev/null 2>&1 || exit 1
 PGUSER="${PGUSER:="postgres"}"
-#IFS=','
-#OLDIFS=$IFS
-#LINEBREAKS=$'\n\b'
 
 DB_LIST=`psql -U "$PGUSER" -F, -Atc "SELECT datname from pg_database where datallowconn"`
 
@@ -13,7 +10,7 @@ print_norm_int() {
 }
 
 for db in $DB_LIST ; do
-vacuum_data=$(psql -U "$PGUSER" -F, -Atc "WITH max_age AS (SELECT 2000000000 as max_old_xid, setting AS autovacuum_freeze_max_age FROM pg_settings WHERE name = 'autovacuum_freeze_max_age'), per_database_stats AS (SELECT datname, m.max_old_xid::int, m.autovacuum_freeze_max_age::int, age(d.datfrozenxid) AS oldest_current_xid FROM pg_database d JOIN max_age m ON (true) WHERE d.datallowconn) SELECT datname, oldest_current_xid, ROUND(100*(oldest_current_xid/max_old_xid::float)) AS percent_towards_wraparound, ROUND(100*(oldest_current_xid/autovacuum_freeze_max_age::float)) AS percent_towards_emergency_autovac FROM per_database_stats WHERE datname = '$db' ORDER BY 3 DESC, 2 DESC, 1 ASC;")
+vacuum_data=$(psql -U "$PGUSER" -F, -Atc "WITH max_age AS (SELECT 2000000000 as max_old_xid, setting AS autovacuum_freeze_max_age FROM pg_settings WHERE name = 'autovacuum_freeze_max_age'), per_database_stats AS (SELECT datname, m.max_old_xid::int, m.autovacuum_freeze_max_age::int, age(d.datfrozenxid) AS oldest_current_xid FROM pg_database d JOIN max_age m ON (true) WHERE d.datallowconn) SELECT 'autovac', max(oldest_current_xid) AS oldest_current_xid, max(ROUND(100*(oldest_current_xid/max_old_xid::float))) AS percent_towards_wraparound, max(ROUND(100*(oldest_current_xid/autovacuum_freeze_max_age::float))) AS percent_towards_emergency_autovac FROM per_database_stats;")
 IFS=','
 DATA=( `echo "${vacuum_data}"` )
 echo -e "${DATA[0]}:oldest_current_xid\tL\t${DATA[1]}"
