26521c07eda283a2020358fcd22afed69acfa31a
Theo Schlossnagle <theo.schlossnagle@circonus.com>
2016-08-11 09:58:05 -0400

    make this work on darwin

--- plugins/common/ps.pl
+++ plugins/common/ps.pl
@@ -15,7 +15,11 @@ my $username_opt = @ARGV ? "-u " . join(',', @ARGV)  : '-e ';
 #   -o pid= -o user= -o s=
 #  above works on solaris and gnu ps
 #  If nonexistent user specified, we get a usage message to STDERR - ignore it
-my $cmd = "ps -o pid= -o user= -o s= $username_opt 2>/dev/null";
+my $state_fmt = "s=";
+if ($^O eq 'darwin') {
+    $state_fmt = "state=";
+}
+my $cmd = "ps -o pid= -o user= -o $state_fmt $username_opt 2>/dev/null";
 
 my %count_by_state = map { $_ => 0 } qw(D R S Z O total); # Others?
 for my $proc (`$cmd`) {
