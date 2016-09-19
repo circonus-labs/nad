1418e60fa3bf4ae2fe646934902567070a9b4343
Riley Berton <riley.berton@circonus.com>
2016-09-19 17:02:55 +0000

    use NADURL instead of NAD_PORT

--- plugins/postgresql/pg_protocol_observer.sh
+++ plugins/postgresql/pg_protocol_observer.sh
@@ -3,7 +3,7 @@ source /opt/circonus/etc/pg-po-conf.sh
 
 which protocol_observer >/dev/null 2>&1 || exit 1
 IFACE="${IFACE:="auto"}"
-NAD_PORT="${NAD_PORT:="2609"}"
+NADURL="${NADURL:="http://localhost:2609"}"
 
 # if protocol_observer is already running, exit
 POPID=`ps ax | grep protocol_observer | grep -v grep`
@@ -11,4 +11,4 @@ if [ -n "$POPID" ]; then
   exit 0;
 fi
 
-sudo protocol_observer -wire postgres -submissionurl http://localhost:${NAD_PORT}/write/postgres_protocol_observer &
+sudo protocol_observer -wire postgres -submissionurl ${NADURL}/write/postgres_protocol_observer &
