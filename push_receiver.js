72d24cb762582d0c0ff307f330eb513f8de11cc0
Riley Berton <riley.berton@circonus.com>
2016-09-19 19:36:19 +0000

    treat push_receiver liked the windowed dtrace_aggr module

--- push_receiver.js
+++ push_receiver.js
@@ -2,51 +2,65 @@ function PushReceiver() {
     this.set = {};
 };
 
+function __aggr(tgt, src) {
+    for(var key in src) {
+        if(!tgt.hasOwnProperty(key)) {
+            tgt[key] = { "_type" : src[key]._type, "_value": []};
+        }
+        if (Array.isArray(src[key]._value)) {
+            for (var v in src[key]._value) {
+                tgt[key]._value.push(src[key]._value[v]);
+            }
+        } else {
+            tgt[key]._value.push(src[key]._value);
+        }
+    }
+}
+
+
 PushReceiver.prototype.run = function(details, cb, req, args, instance) {
     for (var x in this.set) {
-	console.log("Calling data callback with: " + JSON.stringify(this.set[x]));
-	
-	// deep copy the incoming object as we are about to wipe it.
-        cb(details, JSON.parse(JSON.stringify(this.set[x])), x);
-	// wipe all values as we just submitted them
-	// this leaves behind the metric definitions
-	for (var y in this.set[x]) {
-	    this.set[x][y]._value = null;
-	}
+        var o = this.set[x];
+        
+        var period = req ? req.headers['x-reconnoiter-period'] : 0;
+        if(!period) period = 60000;
+        var this_idx = Math.floor((+new Date()) / 1000);
+        var start_idx = this_idx -= Math.floor(period/1000);
+        var s = {};
+        for(var i = start_idx; i <= o.last_idx; i++) {
+            __aggr(s, o.windows[i % 60]);
+        }
+
+        /* parse/stringify for a deep copy */
+        cb(details, JSON.parse(JSON.stringify(s)), x);
     }
     details.running = false;
 };
-
+        
 PushReceiver.prototype.some_data = function(name, data) {
     console.log("Received some_data for [" + name + "]: " + data);
     var x = JSON.parse(data);
-    if (this.set[name] == undefined) {
+
+    if (!this.set.hasOwnProperty(name)) {
         this.set[name] = {};
+        this.set.last_idx = 0;
+        this.set[name].windows = [];
+        for (var i = 0; i < 60; i++) this.set[name].windows[i] = {};
     }
 
-    for (var y in x) {
-	//console.log("Field: " + JSON.stringify(x[y]));
-        if (this.set[name][y] == undefined || this.set[name][y] == null) {
-	    console.log(name + "[" + y + "] is undefined or null");
-            this.set[name][y] = x[y];
-	    //console.log("Set to: " + JSON.stringify(this.set[name][y]));
-	    
-        } else {
-            if(!Array.isArray(this.set[name][y]._value)) { // upgrade to array
-		//console.log("Upgrading to array");
-                this.set[name][y]._value = [ this.set[name][y]._value ];
-            }
-	    if (Array.isArray(x[y]._value)) {
-		//console.log("Incoming data is array");
-		for (var z in x[y]._value) {
-		    this.set[name][y]._value.push(x[y]._value[z]);
-		}
-	    } else {
-		this.set[name][y]._value.push(x[y]._value);
-	    }
-	    console.log("Set to: " + JSON.stringify(this.set[name][y]));
-        }
+    var o = this.set[name];
+
+    /* for each incoming key, save all the data */
+    var this_idx = Math.floor((+new Date()) / 1000);
+    if(!o.last_idx) o.last_idx = this_idx;
+
+    /* everything from last up to now is moot */
+    for(var i = o.last_idx + 1; i < this_idx; i++) {
+      o.windows[i % 60] = {};
     }
+    console.log("Saving incoming " + JSON.stringify(x) + " in " + (this_idx % 60));
+    o.windows[this_idx % 60] = x;
+    o.last_idx = this_idx;    
 };
 
 module.exports = PushReceiver;
