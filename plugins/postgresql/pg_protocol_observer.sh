c04a7e3627a51e8b5c3ffa35ee701d8e311d14d8
Riley Berton <riley.berton@circonus.com>
2016-09-19 17:31:04 +0000

    remove trailing slash from NADURL

--- plugins/postgresql/pg_protocol_observer.sh
+++ plugins/postgresql/pg_protocol_observer.sh
@@ -5,6 +5,8 @@ which protocol_observer >/dev/null 2>&1 || exit 1
 IFACE="${IFACE:="auto"}"
 NADURL="${NADURL:="http://localhost:2609"}"
 
+NADURL=${NADURL%/}
+
 # if protocol_observer is already running, exit
 POPID=`ps ax | grep protocol_observer | grep -v grep`
 if [ -n "$POPID" ]; then
