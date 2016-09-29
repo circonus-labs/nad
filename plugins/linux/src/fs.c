556fb5ed79ce48da4f1aad147c0bd12d852072b7
matt maier <maier@users.noreply.github.com>
2016-09-29 10:07:37 -0400

    fix: fs with f_blocks=0 would result in remaining fs being skipped

--- plugins/linux/src/fs.c
+++ plugins/linux/src/fs.c
@@ -6,10 +6,12 @@
 #include <stdlib.h>
 #include <mntent.h>
 
+// 2016-09-29T13:57:29Z add pstore - https://www.kernel.org/doc/Documentation/ABI/testing/pstore
 static const char *suppress_fstype[] = {
-  "rootfs", "proc", "sysfs", "selinuxfs", "usbfs", "devpts", "devtmpfs", 
+  "rootfs", "proc", "sysfs", "selinuxfs", "usbfs", "devpts", "devtmpfs",
   "binfmt_misc", "rpc_pipefs", "autofs", "debugfs", "securityfs", "fusectl",
   "cgroup", "configfs", "mqueue", "hugetlbfs", "fuse.gvfs-fuse-daemon", "xenfs",
+  "pstore",
   NULL
 };
 
@@ -32,29 +34,38 @@ int main(int argc, char **argv) {
       if(!strcmp(mnt.mnt_type, suppress_fstype[i])) break;
 
     if (suppress_fstype[i] == NULL && statvfs(mnt.mnt_dir, &buf) == 0) {
-      long long unsigned int used = buf.f_blocks - buf.f_bfree,
-                             adj = buf.f_blocks - buf.f_bfree + buf.f_bavail;
-      if (adj == 0) {
-        break;
-      }
-      double pct = (used * 100) / adj + ((used * 100) % adj != 0);
-      printf("%s`f_bsize\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_bsize);
-      printf("%s`f_frsize\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_frsize);
-      printf("%s`f_blocks\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_blocks);
-      printf("%s`f_bfree\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_bfree);
-      printf("%s`f_bavail\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_bavail);
-      printf("%s`df_used_percent\tL\t%0.2f\n", mnt.mnt_dir, pct);
-      printf("%s`used_percent\tL\t%0.2f\n", mnt.mnt_dir, 100.0*(double)used/(double)adj);
-      if(buf.f_files > 0) {
-        used = buf.f_files - buf.f_ffree;
-        pct = (used * 100) / buf.f_files + ((used * 100) % buf.f_files != 0);
-        printf("%s`df_used_inode_percent\tL\t%0.2f\n", mnt.mnt_dir, pct);
-        printf("%s`used_inode_percent\tL\t%0.2f\n", mnt.mnt_dir, 100.0*(double)(buf.f_files - buf.f_ffree)/(double)buf.f_files);
-      }
-      printf("%s`f_files\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_files);
-      printf("%s`f_ffree\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_ffree);
-      printf("%s`f_favail\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_favail);
-    }
+        long long unsigned int used = 0, adj = 0;
+        double pct = 0, df_pct = 0;
+
+        printf("%s`f_bsize\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_bsize);
+        printf("%s`f_frsize\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_frsize);
+        printf("%s`f_blocks\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_blocks);
+        printf("%s`f_bfree\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_bfree);
+        printf("%s`f_bavail\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_bavail);
+        printf("%s`f_files\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_files);
+        printf("%s`f_ffree\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_ffree);
+        printf("%s`f_favail\tL\t%llu\n", mnt.mnt_dir, (long long unsigned int) buf.f_favail);
+
+        pct = 0;
+        df_pct = 0;
+        if (buf.f_blocks > 0) {
+            used = buf.f_blocks - buf.f_bfree;
+            adj = buf.f_blocks - buf.f_bfree + buf.f_bavail;
+            df_pct = (used * 100) / adj + ((used * 100) % adj != 0);
+           	pct = 100.0*(double)used/(double)adj;
+        }
+        printf("%s`df_used_percent\tL\t%0.2f\n", mnt.mnt_dir, df_pct);
+        printf("%s`used_percent\tL\t%0.2f\n", mnt.mnt_dir, pct);
+
+        pct = 0;
+        df_pct = 0;
+        if(buf.f_files > 0) {
+            used = buf.f_files - buf.f_ffree;
+            df_pct = (used * 100) / buf.f_files + ((used * 100) % buf.f_files != 0);
+            pct = 100.0*(double)(buf.f_files - buf.f_ffree)/(double)buf.f_files;
+        }
+        printf("%s`df_used_inode_percent\tL\t%0.2f\n", mnt.mnt_dir, df_pct);
+        printf("%s`used_inode_percent\tL\t%0.2f\n", mnt.mnt_dir, pct);    }
   }
   exit(0);
 }
