b03041795e02e638d2ab45f60ce3d9202da83401
matt maier <maier@users.noreply.github.com>
2016-10-19 13:18:16 -0400

    update: derive plugin directory based on location of script

--- plugins/postgresql/pg_cache.sh
+++ plugins/postgresql/pg_cache.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-plugin_dir="/opt/circonus/etc/node-agent.d/postgresql"
+plugin_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd -P)
 pgfuncs="${plugin_dir}/pg_functions.sh"
 [[ -f $pgfuncs ]] && source $pgfuncs
 [[ ${pg_functions:-0} -eq 0 ]] && { echo "Invalid plugin configuration."; exit 1; }
