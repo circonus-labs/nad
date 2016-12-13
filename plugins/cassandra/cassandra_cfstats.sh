3278e2e9dfbb933252cf8701f7200dc689a60673
matt maier <maier@users.noreply.github.com>
2016-12-13 13:26:41 -0500

    upd: clean up and make uniform

--- plugins/cassandra/cassandra_cfstats.sh
+++ plugins/cassandra/cassandra_cfstats.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-which nodetool >/dev/null 2>&1 || exit 1
-which awk >/dev/null 2>&1 || exit 1
+
+which nodetool &>/dev/null || {
+    >&2 echo "Unable to find 'nodetool' in path"
+    exit 1
+}
 
 nodetool cfstats -F json 2>/dev/null
+
+# END
--- plugins/cassandra/cassandra_compaction.sh
+++ plugins/cassandra/cassandra_compaction.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-which nodetool >/dev/null 2>&1 || exit 1
-which awk >/dev/null 2>&1 || exit 1
-CASS_USER="${CASS_USER:="admin"}"
+
+which nodetool &>/dev/null || {
+    >&2 echo "Unable to find 'nodetool' in path"
+    exit 1
+}
+
+which awk &>/dev/null || {
+    >&2 echo "Unable to find 'awk' in path"
+    exit 1
+}
+
+: ${CASS_USER:=admin}
 
 nodetool compactionstats 2>/dev/null | awk '
 /pending tasks/ { print "pending_tasks\tL\t"$3 }
-' 
+'
 
+# END
