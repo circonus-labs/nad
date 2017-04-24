2a32cca32a898e17995e695f7ceb85dcebf2f1d0
matt maier <maier@users.noreply.github.com>
2017-04-24 11:17:39 -0400

    add: more stringent lint rules

---
+++ sbin/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    extends: [
+        '@maier/base',
+        '@maier/node',
+        '@maier/node-cmd'
+    ],
+    rules: {
+        // ...additional project specific rules
+    }
+};
