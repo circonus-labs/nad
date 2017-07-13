0df54f5829d77233530ed967f2251d8438315e29
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2017-07-13 23:10:59 +0200

    Metric additions for USE dashboard (#106)
    * linux/vm.sh Avoid spawning extra processes when reading /proc/vmstat
    
    * linux/vm.sh: Emit swap metrics
    
    * linux/cpu.sh: Add process statistics
    
    * linux/if.sh Add documentation
    
    * linux/if.sh: Add metrics *drop/*overrun/segments_retransmitted
    
    * linux/if.sh: add metric tcp`connections
    
    * linux/vm.sh: Add corrected {used,free}_percent metric
    
    ... and existing non-normalized percent_{used,free} as is.
    
    * linux/vm.sh: Add page scan metric
    
    * linux/{cpu.sh,vm.sh} Fix exit codes
    
    * Bump version
    
    * Fix metric docs

--- plugins/linux/if.sh
+++ plugins/linux/if.sh
@@ -22,10 +22,50 @@ DEVICES=(
 
 for iface in "${DEVICES[@]}" ; do
     IFSTAT=($(grep $iface /proc/net/dev | tr ':' ' '))
+    # METRIC { name="if`<IF>`in_bytes", desc="Number of received bytes", unit="byte" }
     print_iface $iface in_bytes ${IFSTAT[1]}
+    # METRIC { name="if`<IF>`in_packets", desc="Number of packets received" }
     print_iface $iface in_packets ${IFSTAT[2]}
+    # METRIC { name="if`<IF>`in_errors", desc="Number of bad packets received" }
     print_iface $iface in_errors ${IFSTAT[3]}
+    # METRIC { name="if`<IF>`in_drop", desc="Number of dropped packets due to lack of space in kernel buffers" }
+    print_iface $iface in_drop ${IFSTAT[4]}
+    # METRIC { name="if`<IF>`in_fifo_overrun", desc="Number of fifo overrun errors" }
+    print_iface $iface in_fifo_overrun ${IFSTAT[5]}
+
+    # METRIC { name="if`<IF>`out_bytes", desc="Number of tranmitted bytes", unit="byte" }
     print_iface $iface out_bytes ${IFSTAT[9]}
+    # METRIC { name="if`<IF>`out_packets", desc="Number of outgoing packets" }
     print_iface $iface out_packets ${IFSTAT[10]}
+    # METRIC { name="if`<IF>`out_errors", desc="Number of errors that happend while transmitting packets" }
     print_iface $iface out_errors ${IFSTAT[11]}
+    # METRIC { name="if`<IF>`out_drop", desc="Number of dropped packets due to lack of space in kernel buffers" }
+    print_iface $iface out_drop ${IFSTAT[12]}
+    # METRIC { name="if`<IF>`out_fifo_overrun", desc="Number of fifo overrun errors" }
+    print_iface $iface out_fifo_overrun ${IFSTAT[13]}
 done
+
+# Read segment retransmitted from /proc/net/snmp
+# METRIC { name="if`tcp`segments_retransmitted", descr="Retransmitted tcp segments, systemwide" }
+let ROW=0
+while IFS=":" read HEAD TAIL
+do
+    [[ $HEAD = "Tcp" ]] && let ROW+=1
+    [[ $HEAD = "Tcp" ]] && [[ $ROW -gt 1 ]] && break
+done < /proc/net/snmp
+FIELDS=( $TAIL )
+printf "%s\tL\t%s\n" tcp\'segments_retransmitted ${FIELDS[11]}
+
+# Connection Statistics from /proc/net/socstat{,6}
+# METRIC { name="if`tcp`connections", descr="Number of currently open TCP connections." }
+# It would be much better if this was a counter: "number of connections since boot", so we could calculate #con/sec, etc.
+let CONNECTIONS=0
+while IFS=" " read HEAD inuse COUNT
+do
+    [[ $HEAD == "TCP:" ]] && let CONNECTIONS+=$COUNT && break
+done </proc/net/sockstat
+while IFS=" " read HEAD inuse COUNT
+do
+    [[ $HEAD == "TCP6:" ]] && let CONNECTIONS+=$COUNT && break
+done </proc/net/sockstat6
+printf "%s\tL\t%s\n" tcp\`connections $CONNECTIONS
--- plugins/linux/vm.sh
+++ plugins/linux/vm.sh
@@ -44,15 +44,15 @@ END {
     memFreeTotal=(memFree + memBuffers + memCached)
 
     memUsed=memTotal - memFreeTotal
-    memFreePct=memFreeTotal / memTotal
-    memUsedPct=memUsed / memTotal
+    memFreePct=memFreeTotal / memTotal * 100
+    memUsedPct=memUsed / memTotal * 100
 
     swapTotal=list["SwapTotal"]
     swapFree=list["SwapFree"]
     swapUsed=swapTotal - swapFree
     if (swapTotal > 0) {
-        swapFreePct=swapFree / swapTotal
-        swapUsedPct=swapUsed / swapTotal
+        swapFreePct=swapFree / swapTotal * 100
+        swapUsedPct=swapUsed / swapTotal * 100
     } else {
         swapFreePct=0
         swapUsedPct=0
@@ -67,13 +67,17 @@ END {
     printf("memory`total\tL\t%.0f\n", memTotal)
     printf("memory`used\tL\t%.0f\n", memUsed)
     printf("memory`free\tL\t%.0f\n", memFreeTotal)
-    printf("memory`percent_used\tn\t%.02f\n", memUsedPct)
-    printf("memory`percent_free\tn\t%.02f\n", memFreePct)
+    printf("memory`percent_used\tn\t%.02f\n", memUsedPct / 100) # deprecated
+    printf("memory`percent_free\tn\t%.02f\n", memFreePct / 100) # deprecated
+    printf("memory`used_percent\tn\t%.02f\n", memUsedPct)
+    printf("memory`free_percent\tn\t%.02f\n", memFreePct)
     printf("swap`total\tL\t%.0f\n", swapTotal)
     printf("swap`used\tL\t%.0f\n", swapUsed)
     printf("swap`free\tL\t%.0f\n", swapFreeTotal)
-    printf("swap`percent_used\tn\t%.02f\n", swapUsedPct)
-    printf("swap`percent_free\tn\t%.02f\n", swapFreePct)
+    printf("swap`percent_used\tn\t%.02f\n", swapUsedPct / 100) # deprecated
+    printf("swap`percent_free\tn\t%.02f\n", swapFreePct / 100) # deprecated
+    printf("swap`used_percent\tn\t%.02f\n", swapUsedPct)
+    printf("swap`free_percent\tn\t%.02f\n", swapFreePct)
 }' < $PROCFILE
 
 
@@ -83,11 +87,20 @@ PROCFILE="/proc/vmstat"
     exit 1
 }
 
-# pgfault is min+maj
-PG_FAULTS=$(grep ^pgfault $PROCFILE | $AWK '{ print $2 }')
-PG_MAJFAULTS=$(grep ^pgmajfault $PROCFILE | $AWK '{ print $2 }')
+let PG_SCAN=0
+let PG_FAULTS=0
+let PG_MAJFAULTS=0
+while IFS=" " read NAME VAL
+do
+    [[ "$NAME" = pgfault ]]    && PG_FAULTS="$VAL"
+    [[ "$NAME" = pgmajfault ]] && PG_MAJFAULTS="$VAL"
+    [[ "$NAME" = pswp* ]]      && print_vm vmstat $NAME $VAL
+    [[ "$NAME" = pgscan* ]]    && PG_SCAN=$(($PG_SCAN + $VAL))
+done < $PROCFILE
+
 let PG_MINFAULTS=$PG_FAULTS-$PG_MAJFAULTS
 
 print_vm info page_fault $PG_FAULTS
 print_vm info page_fault\`minor $PG_MINFAULTS
 print_vm info page_fault\`major $PG_MAJFAULTS
+print_vm info page_scan $PG_SCAN
