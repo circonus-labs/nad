9bd1ce6dd7073a36f0b134ebfd481f1c032b7a1a
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2017-07-31 18:03:30 +0200

    linux/bccbpf: Handle setuid errors

--- plugins/linux/bccbpf/iolatency.c
+++ plugins/linux/bccbpf/iolatency.c
@@ -1,11 +1,17 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 //
 // Binary setuid wrapper for iolatency.py
 //
 // We can't use setuid on scripts directly.
 //
 int main() {
-  setuid(geteuid());
+  int rc = setuid(0);
+  if(rc) {
+    fprintf(stderr, "Privilege escalation failed. Is the setuid bit set (chmod u+s iolatency.elf)?\n");
+    return -1;
+  }
   system("/opt/circonus/nad/etc/node-agent.d/linux/bccbpf/iolatency.py");
+  return 0;
 }
